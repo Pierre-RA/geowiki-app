ea562163b903d89de39b6cc97e2666400c965914
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-07-04 17:40:16 +0200

    feat: add a better map

---
+++ README.md
@@ -0,0 +1,18 @@
+Geo-wiki application
+====================
+
+Install
+-------
+
+Create a file in `app/App_Resources/values/nativescript_google_maps_api.xml` with this content inside:
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="nativescript_google_maps_api_key">YOUR_API_KEY</string>
+</resources>
+```
+
+```
+$ tns run android --device DEVICE_NUMBER
+```
--- app/pages/map/map.component.ts
+++ app/pages/map/map.component.ts
@@ -1,9 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { registerElement } from "nativescript-angular/element-registry";
 import { MapView, Marker, Position } from 'nativescript-google-maps-sdk';
+import * as geolocation from "nativescript-geolocation";
+
+import * as fs from "tns-core-modules/file-system";
 
 registerElement('MapView', () => MapView);
-// registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
 
 @Component({
   moduleId: module.id,
@@ -13,11 +15,50 @@ registerElement('MapView', () => MapView);
 })
 export class MapComponent {
 
-  @ViewChild("MapView") mapView: ElementRef;
+  // @ViewChild("MapView") mapView: ElementRef;
+  mapView: MapView;
+  public activityList: Array<Object> = [];
+  latitude =  -33.86;
+  longitude = 151.20;
+  zoom = 14;
 
   //Map events
   onMapReady = (event) => {
     // this.mapView.setStyle();
-    console.log("Map Ready");
+
+    this.mapView = event.object;
+
+    // Load mockup document
+    var documents = fs.knownFolders.documents();
+    var myFile = documents.getFile("app/mockup/activities.json");
+    let activityList = [];
+    myFile.readText()
+      .then(content => {
+        try {
+          let json = JSON.parse(content);
+          json.results.forEach(entity => {
+            entity.gps = entity.latitude + ' x ' + entity.longitude;
+            entity.text = entity.description.en || 'no description';
+            this.activityList.push(entity);
+          });
+          let temp;
+          // Show Saint-Raphael
+          if (true) {
+            temp = this.activityList[1];
+            console.log(temp);
+          }
+
+          var marker = new Marker();
+          marker.position = Position.positionFromLatLng(temp.latitude, temp.longitude);
+          marker.title = temp.place;
+          marker.snippet = temp.text;
+          marker.userData = {index: 1};
+          this.mapView.addMarker(marker);
+        } catch (err) {
+          throw new Error('Could not parse JSON file. 1.' + err);
+        }
+      }, function (error) {
+        throw new Error('Could not read JSON file. 2.');
+      });
   };
 }
